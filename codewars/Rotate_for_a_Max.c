86a3464f380d2df40e399b2ec50de4fb115a651e
WilhelmSonne55 <r03941114@ntu.edu.tw>
2017-08-13 16:06:59 +0800

    Update Rotate_for_a_Max.c

--- codewars/Rotate_for_a_Max.c
+++ codewars/Rotate_for_a_Max.c
@@ -1,19 +1,18 @@
-long long maxRot(long long n) {
-  long long t_digit = 10;
-  long long tmp = n, max=n;
-  
-  while(t_digit<n) t_digit *= 10;  
-  t_digit /=10;
+long long maxRot(long long n){
+    long long tmp =n, max = n;
+    long long digit = 1;
 
-  while(t_digit !=0 ){
-        long long k_digit = t_digit * 10;
-        long long bottom_n = tmp%t_digit;
-        long long top_n = (tmp/t_digit)%10;
-        long long k_n = tmp/k_digit;
-        tmp = k_n*k_digit + bottom_n*10 + top_n;
-        t_digit /= 10;
+    while(digit*10 < n) digit *=10;       //get the digit of the n
+
+    while(digit !=0 ){
+        long long k_digit = digit*10;     //the digit of the number we keep 
+        long long k_n = tmp / k_digit;    //the number we kepp
+        long long b_n = tmp % digit;      //bottom number should shift one bit
+        long long t_n = (tmp/digit)%10;   //origin top number => first bit
+
+        tmp = k_n * k_digit + b_n*10 + t_n; // xx (k_n) 0 (t_n) xxxx (b_n) 0 <==== move to here
+        digit /= 10;
         max = tmp>max?tmp:max;
-  }
-  
-  return max;
+    }
+    return max;
 }
