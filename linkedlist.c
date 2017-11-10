693771eadb11677c06a2f3a14fe7aeb79b423bad
WilhelmSonne55 <r03941114@ntu.edu.tw>
2017-11-10 20:59:18 +0800

    Update linkedlist.c
    insert
    delete
    reverse
    compare

--- linkedlist.c
+++ linkedlist.c
@@ -1,95 +1,164 @@
 #include<stdio.h>
-#include<stdlib.h>
 
-struct node{
-	struct node* next;
+typedef struct node{
 	int data;
-	node(){
-		data= 0;
-		next = NULL;
-	}
-
-};
-
-
-struct linklist{
-	struct node* last;
-	struct node* first;
-	int size = 0;
+	struct node* next;
 	
-	/*push into the middle of link list  */
-	void push(int n, int data){
-		
-		if(n > size){	//check if n > actual linklist size
-			printf("out of the size of the link list\n");
+}Node;
+
+typedef struct linkedlist{
+	Node* head;
+	
+}List;  //only this ?
+
+Node* createNode(int data){
+	Node* tmp = (Node*)malloc(sizeof(Node));
+	tmp->data = data;
+	tmp->next =NULL;
+	return tmp;
+}
+
+Node* insert(Node* head, Node* node, int data){ //insert next to node
+	Node* tmp = createNode(data);
+	
+	if(node == NULL){ // insert 0st
+		tmp->next = head;
+		head = tmp;
+	}else{
+		if(node->next !=NULL){ //middle
+			tmp->next = node->next ;
+			node->next = tmp;
 		}
 		else{
-			struct node* tmp = (struct node*)malloc(sizeof(struct node));
-			tmp->data = data;
-			tmp->next = NULL;
-				
-			if(0 < size){	//if size=1 first=last, else....
-				struct node* tmp2 = first;
-				++size;
-
-				if(size == 2){	//if size=2, last must the new one
-					tmp->next = tmp2->next;
-					tmp2->next = tmp;
-					last = tmp;
-					
-				}else{	//last might be the new one's next
-					int i = 1;	//i : size 1. 2. 3. 4.....not from 0
-					while(tmp2->next !=NULL){	//find position of insertion, and find last one
-						if(i == n){
-							tmp->next= tmp2->next;
-							tmp2->next = tmp;
-						}												
-						tmp2 = tmp2->next;
-						last = tmp2;
-						++i;
-					}
-				}	
-			}
-			else{
-				++size;
-				first = tmp;
-				last = tmp;
-			}	
-		}	
-		
-		printAll();
-	}
-	
-	void printAll(){
-		struct node* tmp = first;
-		int i=0;
-		printf("\n size:%d :",size);
-		while(i < size){
-			printf(" %d ", tmp->data);
-			tmp = tmp->next;
-			++i;
+			node->next = tmp; //last one
 		}
-		
 	}
-	
-	
-	
-};
+	return head;
+}
 
 
-int main(int argc, char*argv[]){
-	struct linklist link;
-	//link.push(0,5);
-	link.push(0,6);
-	link.push(1,5);
-	link.push(1,9);
-	// link.push(3,8);
+//without return Node* head => Node** head
+void insertNode(Node** head, Node* ptr, Node* node){
+	
+	if(ptr == NULL){
+		node->next=*head;
+		*head = node;
+	}else{
+		if(ptr->next ==NULL){
+			ptr->next = node;
+		}else{
+			node->next = ptr->next;
+			ptr->next = node;
+		}
+	}	
+}
 
-	// printf(" %d \n",link.last->data);
-	// printf(" %d ",link.size);
-	//link.printAll();
+//without return Node* head => Node** head
+void deleteNode(Node** head, Node* node){
+	Node* tmp = *head; 
+
+	if(node == *head){ //first 
+		*head = node->next;
+	}
+	else{
+		while(tmp->next != node){ //last
+			tmp=tmp->next;
+		
+		}
+		if(node->next !=NULL){ //middle
+			tmp->next = node->next;
+		}else{
+			tmp->next =NULL; //last
+		}
+	}
+	free(node);
+}
+
+void print(Node* head){
+	Node* tmp = head;
+	while(tmp->next !=NULL){
+		printf("%d \n", tmp->data);
+		tmp = tmp->next;
+	}
+	printf("%d \n\n", tmp->data);
+}
+
+void reverse(Node** head){
+	Node* next = NULL;
+	Node* cur = *head;
+	Node* tmp = NULL;
 	
+	while(cur !=NULL){
+		next = cur->next;
+		cur->next = tmp;
+		tmp = cur;
+		cur = next;
+	}
+	*head = tmp;
+}
+
+//return Node* using Node* head
+Node* reverseNode(Node* head){
+	Node* tmp = NULL;
+	Node* next;
+	Node* cur = head;
+	while(cur !=NULL){
+		next = cur->next;
+		cur->next = tmp;
+		tmp = cur;
+		cur = next;
+	}
+	return tmp;
+}
+
+//compare
+int compareList(Node* list1, Node* list2){
 	
+	static int flag;
+	if(list1 == NULL && list2 == NULL) //the same
+		flag=1;
+	else{
+		if(list1 == NULL || list2 == NULL) //one of lists end
+			flag=0;
+		else if(list1->data != list2->data) //different
+			flag=0;
+		else
+			compareList(list1->next, list2->next); 
+	}
+	return flag;
+}
+
+int main(int argc, char** argv){
+	Node* list = createNode(3);
 	
+	list = insert(list, 0, 5);
+	
+	Node* a = createNode(5);
+	Node* b = createNode(4);
+	insertNode(&list,0,a);
+	insertNode(&list,a,b);
+
+	print(list);
+	
+	deleteNode(&list,a);
+	print(list);
+	
+	/*reverse*/
+	reverse(&list);
+	print(list);
+	
+	/*reverse*/
+	list = reverseNode(list);
+	print(list);
+	
+	//compare
+	Node* list2 = createNode(4);
+	Node* c = createNode(5);
+	Node* d = createNode(3);
+	insertNode(&list2,list2,c);
+	// insertNode(&list2,c,d);
+	print(list2);
+	printf(" %d \n",compareList(list,list2));
+
 	return 0;
 }
